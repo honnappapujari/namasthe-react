acd28d8680ee59ad9c8e51c86b75b08bfd5035d6
honnappapujari <pujarihonnappa888@gmail.com>
2024-06-01 22:26:33 +0530

    #

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ import React from "react";
 import  ReactDOM  from "react-dom/client";
 
 // const createEl = React.createElement('h1', {id:'heading'}, 'Helo world 🚀');
-const createEl = (<h1 
+const CreateEl = () => (
+<h1 
 id="heading" 
-className="red-COlor">Hello World</h1>);
+className="red-COlor">Hello World 😃</h1>
+);
 
+const price = 10000;
+// !Note : Component Composation
 const Header = () => (
     <>
+    <h2>{price + 100}</h2>
+    <CreateEl />
     <h1>Heloo India</h1>
     <p>Jai Karnataka</p>
     </>
 )
-console.log(createEl);
 
 const rootEl = ReactDOM.createRoot(document.getElementById('root'));
 
