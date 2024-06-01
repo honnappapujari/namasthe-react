e4236e8807611da63ac0b3189b5828001bd58fa2
honnappapujari <pujarihonnappa888@gmail.com>
2024-06-01 20:49:45 +0530

    #

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
-const createEl = React.createElement('h1', {id:'heading'}, 'Helo world 🚀');
+// const createEl = React.createElement('h1', {id:'heading'}, 'Helo world 🚀');
+const createEl = (<h1 
+id="heading" 
+className="red-COlor">Hello World</h1>);
+
+console.log(createEl);
 
 const rootEl = ReactDOM.createRoot(document.getElementById('root'));
 
